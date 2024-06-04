8d44c25a516ab52de5609d8719874a3fd24613e6
ihorkoi <koleev.igor@gmail.com>
2024-06-04 11:53:47 +0300

    change secret

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f$!fs9121j80e6xj-bi4_d@1)*cud4neyyn2$_7_&@cyxpn6a7'
+# SECRET_KEY = ''
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
